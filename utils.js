66dd13f08d4565bda6cea0c960936fbad87b1f96
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-06-14 13:09:50 -0400

    Creating a Beat Class to Play audio files

--- utils.js
+++ utils.js
@@ -4,6 +4,27 @@
  * HINT: Create a play function to play the audio if called 
  */
 class Beat {
+    // In order to initialize something we have to use a constructor
+    // this accepts a string that is the audio link
+    constructor(audioSrc) {
+
+        // here we'll create a new object
+        this.audio = new Audio(audioSrc)
+
+        // testing the program 
+        console.log(this.audio)
+    }
+
+    // Next we'll create a function that would play the audio
+    play = () => {
+        // if you press the button twice every single time it will reset
+        // audio and repeat it again.
+        this.audio.currentTime = 0;
+
+        // this would play the audio file
+        this.audio.play()
+    }
+
 }
 
 
